aa4b989f1fc9b7965fdb60778977f9ab3ec88183
rodrigoestef <rodrigoestef@gmail.com>
2020-07-13 15:53:02 -0300

    tela de visualização

--- src/routes/watch/index.js
+++ src/routes/watch/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {useHistory} from 'react-router-dom'
 import { useUrl } from "../../context/urlContext";
+import { Search } from "./styles";
 
 export default () =>{
-    const {url} = useUrl()
+    const {url,setUrl} = useUrl()
+    const [text,setText] = useState('')
+    const history = useHistory()
+    const videoId = url.split('v=')[1]
+    if (!videoId) {
+        history.goBack()
+    }
+
+    const click = ({key,target}) =>{
+        if (key == 'Enter') {
+            setUrl(target.value)
+        }
+    }
+
+    useEffect(()=>{
+        setText(url)
+    },[])
+
     return(
-        <h1>{url}</h1>
+        <>
+            <Search value ={text} onChange={e=> setText(e.target.value)} onKeyUp={e => click({key: e.key,target:e.target})}/>
+            <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${videoId}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </>
     )
 }
---
+++ src/routes/watch/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export {Search}
+
+
+const Search  = styled.input`
+    background-color:#16171A;
+    color:white;
+    width:100%;
+    /* height: 20px; */
+    border: 0px;
+    /* border-radius:10px; */
+`
