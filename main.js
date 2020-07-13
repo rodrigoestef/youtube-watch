b5f92efdf95d575860986099c9bc8975e068506d
rodrigoestef <rodrigoestef@gmail.com>
2020-07-13 16:40:04 -0300

    adc de icon  mudança de nome e uso do path para arquivos externos no electron

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const {app,BrowserWindow} = require('electron')
+const path = require('path')
 
 const createWindow = () =>{
     const win = new BrowserWindow({width:400,height:200,alwaysOnTop:true,show:false})
-    win.loadFile('./index.html')
+    win.setIcon(path.resolve(__dirname,'icon.png'))
+    win.loadFile(path.resolve(__dirname,'index.html'))
     win.setMenu(null)
     win.once('ready-to-show',()=> win.show())
 
